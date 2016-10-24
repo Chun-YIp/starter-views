815f3f62bee5f9cfcbe770f0ecf419bc1ca0fe75
Chun-Yip <koolman987654321@hotmail.com>
2016-10-23 17:18:32 -0700

    Finished Job3. Need ML to test because failing bitnami

---
+++ application/controllers/Shopping.php
@@ -0,0 +1,29 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Shopping extends Application {
+    
+    function __construct() {
+        parent::__construct();
+    }
+    
+    public function index() {
+        $stuff = file_get_contents('../data/receipt.md');
+        $this->data['receipt'] = $this->parsedown->parse($stuff);
+        
+        $count = 1;
+        foreach ($this->categories->all() as $category) {
+            $chunk = 'category' . $count; 
+            $this->data[$chunk] = $this->parser->parse('category-shop',$category,true);
+            foreach($this->menu->all() as $menuitem) {
+                if ($menuitem->category != $category->id) continue;
+                $this->data[$chunk] .= $this->parser->parse('menuitem-shop',$menuitem,true);
+            }
+            $count++;
+        }
+               
+        $this->data['content'] = '';
+        $this->render('template-shopping');
+    }
+}
